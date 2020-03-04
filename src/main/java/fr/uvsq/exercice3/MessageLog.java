e69084c7dc262b3b138198c24a63339692d6e46f
YasmineRabahallah <yasminerabahallah914@gmail.com>
2020-03-04 01:00:22 +0100

    creation interface messageLOG

---
+++ src/main/java/fr/uvsq/exercice3/MessageLog.java
@@ -0,0 +1,18 @@
+package fr.uvsq.exercice3;
+
+public interface MessageLog {
+
+/**
+ * pour afficher un métier.
+ */
+public interface AffichageMetier {
+	/**
+	 * affichage au début.
+	 */
+	void afficheDebut();
+	/**
+	 * affichage à la fin.
+	 */
+	void afficheFin();
+}
+}
